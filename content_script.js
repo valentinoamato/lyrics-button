b2569658e88c2065c54b92400223abf0397e245c
Valentino Amato <valentinoamatoroberts@gmail.com>
2024-04-15 08:59:38 -0300

    Prevent multiple buttons from being added

--- content_script.js
+++ content_script.js
@@ -2,19 +2,26 @@
 function add_button() {
 
     console.log('Adding button');
+    let button = document.getElementById('lyrics-button');
+    if (button) {
+        return 'Button already added';
+    } 
+    button = document.createElement('button');
     const ownerDiv = document.getElementById('owner'); 
-    const button = document.createElement('button');
     button.textContent = "Lyrics"
+    button.id = "lyrics-button"
 
     ownerDiv.appendChild(button);
+
+    return 'Button added';
 }
 
 // Listen for messages from the background script
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'add_button') {
         // Call the add_button function
-        add_button();
+        const status = add_button();
         // Send a response back to the background script (
-        sendResponse({ status: 'Button added' });
+        sendResponse({ status: status});
     }
 });
